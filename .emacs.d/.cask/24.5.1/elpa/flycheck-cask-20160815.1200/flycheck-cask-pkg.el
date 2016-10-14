d282e616ee36d0c51aacbe2883fccecffc089334
hal0taso <yuya7719guitar@gmail.com>
2016-10-14 11:28:29 +0900

    non-fixed update

--- .emacs.d/.cask/24.5.1/elpa/flycheck-cask-20160815.1200/flycheck-cask-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flycheck-cask" "20160815.1200" "Cask support in Flycheck" '((emacs "24.1") (flycheck "0.14") (dash "2.4.0")) :url "https://github.com/flycheck/flycheck-cask" :keywords '("tools" "convenience"))
--- .emacs.d/.cask/24.5.1/elpa/yatex-20160813.100/yatex-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "yatex" "20160813.100" "Yet Another tex-mode for emacs //野鳥//" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
