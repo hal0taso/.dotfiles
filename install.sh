63d4378f8e9a48ca5d922eb4595d0af610006b97
hal0taso <yuya7719guitar@gmail.com>
2017-06-11 05:08:53 +0900

    [add file] add install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export PLATFORM='uname'
+
+function log {
+    echo "-> " $1
+}
+
+
+if [ "$PLATFORM" = "Darwin" ]; then 
+    
+    
+fi
+
+
+if [ "$PLATFORM" = "Linux" ]; then 
+    # for linux environments
+    
+fi
+
