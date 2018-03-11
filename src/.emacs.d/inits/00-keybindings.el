964e941b345cbd8f11fd7dc9128eb6db14b5b169
hal0taso <yuya7719guitar@gmail.com>
2018-03-11 18:54:09 +0900

    [UPDATE] init.el and mozc settings

--- src/.emacs.d/inits/00-keybindings.el
+++ src/.emacs.d/inits/00-keybindings.el
@@ -1,1 +1,20 @@
+;; C-h to backspace and C-? to help command
+(keyboard-translate ?\C-h ?\C-?)
+(global-set-key (kbd "C-?") 'help-for-help)
+
+
+;; you can move buffer using arrow-key
+(defun ignore-error-wrapper (fn)
+  "Funtion return new function that ignore errors.
+   The function wraps a function with `ignore-errors' macro."
+  (lexical-let ((fn fn))
+    (lambda ()
+      (interactive)
+      (ignore-errors
+        (funcall fn)))))
+
+(global-set-key [s-left] (ignore-error-wrapper 'windmove-left))
+(global-set-key [s-right] (ignore-error-wrapper 'windmove-right))
+(global-set-key [s-up] (ignore-error-wrapper 'windmove-up))
+(global-set-key [s-down] (ignore-error-wrapper 'windmove-down))
 
